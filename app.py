bf3287f4b391d54c762fdfcf8e952c3a6fc89ac7
TAKAHIRO <kanemaru-t@micromedia.network>
2025-08-16 18:11:36 +0900

    first commit.

---
+++ app.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import SystemMessage, HumanMessage
+import streamlit as st
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
+
+st.title("課題用フォームの作成webアプリ")
+st.write("このアプリは、OpenAIのAPIを使用して、課題用のフォームを作成するためのものです。")
+st.write("以下のフォームに必要な情報を入力してください。")
+
+selected_item = st.radio("選択してください", ["ITの専門家", "料理の専門家", "数学の専門家"])
+st.divider()
+
+input_text = st.text_input(label=f"{selected_item}に関する情報を入力してください")
+
+
+def get_ai_response(selected_item, input_text):
+    role_dict = {  # 選択肢ごとにAIの役割を指定する辞書
+        "ITの専門家": "あなたはITの専門家です。",
+        "料理の専門家": "あなたは料理の専門家です。",
+        "数学の専門家": "あなたは数学の専門家です。"
+    }
+    messages = [
+        SystemMessage(content=role_dict[selected_item]),
+        HumanMessage(content=input_text)
+    ]
+    response = llm(messages)
+    return response.content
+
+
+if st.button("送信"):
+    st.divider()
+    if input_text:
+        answer = get_ai_response(selected_item, input_text)
+        st.write("AIの回答:")
+        st.write(answer)
+    else:
+        st.write(f"{selected_item}に関する情報を入力してください。")
